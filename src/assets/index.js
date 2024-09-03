7044d36c70dce5825bec92b4bdd33726c3e0beba
Ujjwal Kumar <er.ujjwalkumar18@gmail.com>
2024-09-03 18:04:47 +0530

    Final commit of my 3D portfolio project

---
+++ src/assets/index.js
@@ -0,0 +1,63 @@
+import logo from "./logo.svg";
+import frontend from "./frontend.png";
+import design from "./design.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import sql from "./tech/sql.png"
+import java from "./tech/java.png"
+
+
+import celebal from "./company/celebal.png";
+import internshala from "./company/internshala.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  frontend,
+  design,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  sql,
+  java,
+  celebal,
+  internshala,
+  carrent,
+  jobit,
+  tripguide,
+};
